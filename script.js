dce2d64c48dfeac191d31629b5e079f9f46a90df
Evija Fokrote <evija.fokrote@gmail.com>
2022-08-23 19:08:52 +0300

    Update script.js
    change the filtered object to be sortedList rather than myList

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
     const myList = [ ];
-    const sortedList = []
+    const sortedList = [];
+    
 
     const getData = async () => {
         await axios.get('https://dummyjson.com/carts').
@@ -17,7 +18,7 @@ import axios from "axios";
             });
             sortedList.sort((a, b) => a.price - b.price);
             console.log(sortedList);
-            let filteredArray = sortedList.filter(product =>product?.title.includes("oil"));
+            const filteredArray = sortedList.filter(product =>product?.title.toLowerCase().includes("oil"));
             console.log(filteredArray);
             console.log(myList);
         }
